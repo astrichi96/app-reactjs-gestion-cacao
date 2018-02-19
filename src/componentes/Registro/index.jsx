b17eb0c032254130279b6c2193cd7dbb69c83183
Astrid <astrichi96@gmail.com>
2018-02-19 10:45:31 -0500

    Agregando registro de asociacion

--- src/componentes/Registro/index.jsx
+++ src/componentes/Registro/index.jsx
@@ -1,39 +1,107 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
+import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
+import style from './registro.css'
+import Card, { CardContent } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+import Divider from 'material-ui/Divider';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit*2,
+    marginRight: theme.spacing.unit*2,
+    width: 300,
+  }, 
+  card: {
+    maxWidth: 700,
+    textAlign: 'center',
+  },
+   title: {
+    marginBottom: 16,
+    fontSize: 16,
+    color: theme.palette.text.secondary,
+  },
+  button1: {
+    textAlign: 'left' 
+  }
+});
 
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  flex: {
-    flex: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
+class Registro extends React.Component {
+  render() {
+    const { classes } = this.props;
 
-function Registro(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="title" color="inherit" className={classes.flex}>
-            Farmer ORG
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+    return (
+      <div className = {style.container}>
+       <Card className={classes.card}>
+        <CardContent>
+        <Typography component="h2" className={classes.title}>Datos basicos</Typography>
+        <Divider />
+          <br />
+         <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            required
+            id="name"
+            label="Nombre de la Asociación"
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            required
+            id="representante"
+            label="Representante Legal"
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            required
+            id="objetivo"
+            label="Objetivo Social"
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            required
+            id="ubicacion"
+            label="Ubicacion"
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            required
+            id="contacto"
+            label="Numero de Contacto"
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            required
+            id="email"
+            label="Email"
+            className={classes.textField}
+            margin="normal"
+          />
+        </form>
+        <div>
+           <br/><br/>
+           <Button variant="raised" className={style.buttonborrar}>
+            Borrar todo
+           </Button>
+           <Button variant="raised" color="primary" className={style.buttonenviar}>
+            Primary
+          </Button>
+        </div>
+        </CardContent>
+      </Card>
+     </div>
+    )
+  }
 }
 
 Registro.propTypes = {
